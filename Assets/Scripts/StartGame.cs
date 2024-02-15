012197a250b13d9226d6202a752dea7874238eec
Roshankumar Loganathan <82709418+LRKROSHAN@users.noreply.github.com>
2024-02-15 08:40:44 -0500

    Update StartGame.cs

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,19 +1,3 @@
-// using UnityEngine;
-// using UnityEngine.SceneManagement;
-
-// public class StartGame : MonoBehaviour
-// {
-//     void Update()
-//     {
-//         // Check if the Enter key is pressed
-//         if (Input.GetKeyDown(KeyCode.Return))
-//         {
-//             // Load the Game scene
-//             SceneManager.LoadScene("Game");
-//         }
-//     }
-// }
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -21,18 +5,14 @@ public class StartGame : MonoBehaviour
 {
     void Update()
     {
-        // Check if the Return key is pressed
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            // Determine the current scene
             Scene currentScene = SceneManager.GetActiveScene();
 
-            // If the current scene is GameStart, load the Game scene
             if (currentScene.name == "GameStart")
             {
                 SceneManager.LoadScene("Game");
             }
-            // If the current scene is GameOver, load the GameStart scene
             else if (currentScene.name == "GameOver")
             {
                 SceneManager.LoadScene("GameStart");
