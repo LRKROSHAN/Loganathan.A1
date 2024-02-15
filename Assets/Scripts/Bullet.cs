658d2ec2046bd50fdee7ac458ec45a72292e51d7
Roshankumar Loganathan <82709418+LRKROSHAN@users.noreply.github.com>
2024-02-15 08:38:41 -0500

    Update Bullet.cs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,29 +1,9 @@
-// using UnityEngine;
 
-// public class Bullet : MonoBehaviour
-// {
-//     public float speed = 30f; // Speed of the bullet
-
-//     void Update()
-//     {
-//         // Move the bullet forward
-//         transform.Translate(Vector3.forward * speed * Time.deltaTime);
-//     }
-
-//     private void OnTriggerEnter(Collider other)
-//     {
-//         if (other.CompareTag("Enemy")) // Make sure your enemy GameObjects have the tag "Enemy"
-//         {
-//             Destroy(other.gameObject); // Destroy the enemy
-//             Destroy(gameObject); // Destroy the bullet
-//         }
-//     }
-// }
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 30f; // Speed of the bullet
+    public float speed = 30f; 
 
     void Update()
     {
@@ -34,17 +14,14 @@ public class Bullet : MonoBehaviour
     {
         if (other.CompareTag("Enemy"))
         {
-            Destroy(other.gameObject); // Destroy the enemy
-            Destroy(gameObject); // Destroy this bullet
-            FindObjectOfType<EnemyGridSpawner>().EnemyDestroyed(); // Notify the spawner that an enemy has been destroyed
+            Destroy(other.gameObject); 
+            Destroy(gameObject); 
+            FindObjectOfType<EnemyGridSpawner>().EnemyDestroyed(); 
         }
-        // else if (other.CompareTag("Shield")) // React to shield 
-        // {
-        //     Destroy(gameObject); // Destroy this bullet if it hits a shield or an enemy bullet
-        // }
-        else if (other.CompareTag("EBullet")) // React to enemy bullets
+       
+        else if (other.CompareTag("EBullet")) 
         {
-            Destroy(gameObject); // Destroy this bullet if it hits a shield or an enemy bullet
+            Destroy(gameObject);
             Destroy(other.gameObject);
         }
     }
